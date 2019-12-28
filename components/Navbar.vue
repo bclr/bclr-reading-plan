278d8364ea7ebe59df5f22df93e6e7643c425c55
bmeadors <bmeadors@MTI-ATPLT1>
2019-12-27 20:30:24 -0600

    Moved nav to its own component

---
+++ components/Navbar.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  b-navbar(toggleable="lg", fixed="top", variant="light")
+    b-navbar-brand.navbar-brand(href="/")
+      logo
+      | {{ $t('title') }}
+    b-navbar-toggle(target="nav-collapse")
+    b-collapse#nav-collapse(is-nav)
+      social
+    ul.navbar-nav.flex-row
+      li.nav-item
+        a.nav-link.px-1(href="#") About
+</template>
+
+<script>
+export default {
+}
+</script>
