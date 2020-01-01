f0f04601cf613b317f2163a11e7647723477dbf7
bmeadors <bmeadors@MTI-ATPLT1>
2020-01-01 14:25:56 -0600

    Sections

--- pages/primer.vue
+++ pages/primer.vue
@@ -2,7 +2,7 @@
   .container-fluid.bg-bclr.bg-blue
     .row
       .col-12.text-light
-        .section
+        .section.fp-auto-height
           h2 How to Read &ndash; Primer
 
           blockquote.blockquote.text-center
@@ -16,15 +16,14 @@
 
           p The method of reading God’s word with benefit can be reduced to 3 basic questions that can be applied to any reading: 1) What’s the big idea? 2) What are the little details? and 3) Why should I care? Let’s briefly take each in turn.
 
-        .section
+        .section2.fp-auto-height
           p.lead WHAT’S THE BIG IDEA?
           p To answer this question is to take the passage as a whole and attempt to summarize it in as brief and simple a way as possible. Scholars such as R.C. Sproul have said that if a teacher can’t explain a text in terms a child would understand then they still don’t understand it well themselves. “What’s the Big Idea?” asks what is the main point or thrust of the text for the original hearers or readers. Answering this question is at once simple and profound; all it attempts to do is understand the basic message of the text. But in doing so, we (the readers) must distinguish between points, words, terms and ideas that are more central to the meaning from those which are less central. This leads us to the next question.
 
-        .section
           p.lead WHAT ARE THE LITTLE DETAILS?
           p Whichever part of the Bible you find yourself reading, you cannot construct the Big Idea without understanding the individual pieces that make it up! The more your Big Idea makes sense of the individual words, phrases, sentences and paragraphs that make it up, the more confident you can be that you are understanding God’s Word accurately. But don’t restrict yourself only to the immediate context. Scripture refers to itself often, and details in one passage might remind you of the big idea or a little detail from another passage. The more you read in this way, the more you will see how Scripture presents a unified message about Christ. But the Scriptures do it in such a way that individual passages, letters, songs and stories enhance each others’ brilliance as they show us God’s glory.
 
-        .section
+        .section.fp-auto-height
           p.lead WHY SHOULD I CARE?
           p Although we believe that all Scripture is profitable and useful (2 Tim. 3:16-17), sometimes it’s hard to see how! No matter where you are reading in Scripture, there is always something there to contribute to the renewing of your mind (Rom. 12:1-2). Here are some basic questions to ask yourself based on the text.
 
@@ -33,7 +32,6 @@
           p …my desires, values, feelings or emotions?
           p …what I decide, will, intend, commit or act?
 
-        .section
           p
           | As we begin reading together in 2020, let’s follow David’s lead and cover all of our time in God’s Word with humble and sincere prayer:
           | Let the words of my mouth and the meditation of my heart
